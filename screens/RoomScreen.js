4e12d2f1ca8004f9d440f8d6efedc4e6d62b8f14
ShivamGoyal26 <officialshivamgoyal@gmail.com>
2021-02-05 13:02:49 +0530

    Image Selected from the gallery done

--- screens/RoomScreen.js
+++ screens/RoomScreen.js
@@ -1,37 +1,34 @@
-import React, { useState, useContext, useEffect } from 'react';
-import { StyleSheet, View } from 'react-native';
-import { GiftedChat, Composer } from 'react-native-gifted-chat';
+import React, { useState, useContext, useEffect, Fragment } from 'react';
+import { StyleSheet, View, SafeAreaView, Text, TouchableOpacity, Image } from 'react-native';
+import {
+    GiftedChat,
+    Composer,
+    Actions,
+    ActionsProps,
+} from 'react-native-gifted-chat';
 import { AuthContext } from '../Navigation/AuthProvider';
 import firestore from '@react-native-firebase/firestore';
 
-import ImagePicker from 'react-native-image-picker';
+import { launchCamera, launchImageLibrary } from 'react-native-image-picker';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 export default function RoomScreen({ route }) {
 
     // This is for the image 
 
-    const [image, setImage] = useState(null);
-    const [transferred, setTransferred] = useState(0);
-    const [uploading, setUploading] = useState(false);
+    const [filePath, setFilePath] = useState(null);
+    const [fileData, setFileData] = useState(0);
+    const [fileUri, setFileUri] = useState(false);
 
     // This is the for the message 
 
     const { user } = useContext(AuthContext);
     const currentUser = user.toJSON();
     const oppositeUserId = route.params.item.email;
-
-
     const [messages, setMessages] = useState(null);
 
-    // renderComposer = props => {
-    //     return (
-    //       <View style={{flexDirection: 'row'}}>
-    //         <Composer {...props} />
-    //         <CustomImageButton />
-    //         <CustomAttachButton />
-    //       </View>
-    //     );
-    //   }
+    // Adding the image to the cloud firestore 
+
 
     async function addImage(image) {
         const array1 = [currentUser.email, oppositeUserId]
@@ -39,21 +36,22 @@ export default function RoomScreen({ route }) {
         newId = array1[0] + array1[1];
 
         await firestore()
-        .collection('Chats')
-        .doc(newId)
-        .collection('Chat')
-        .add(
-            {
-                image,
-                createdAt: new Date().getTime(),
-                user:{
-                    _id: currentUser.uid,
-                    email: currentUser.email,
+            .collection('Chats')
+            .doc(newId)
+            .collection('Chat')
+            .add(
+                {
+                    image,
+                    createdAt: new Date().getTime(),
+                    user: {
+                        _id: currentUser.uid,
+                        email: currentUser.email,
+                    }
                 }
-            }
-        )
+            )
     }
 
+    // Storing the message to the cloud firestore  
     async function handleSend(message) {
 
         var text = message[0].text;
@@ -76,6 +74,8 @@ export default function RoomScreen({ route }) {
             })
 
     }
+
+    // Fetching all the messages from the cloud firestore 
 
     useEffect(() => {
 
@@ -113,6 +113,58 @@ export default function RoomScreen({ route }) {
         return () => messagesListener();
     }, []);
 
+    // This will help the user to select the image for the uploading 
+
+    const launch = () => {
+        let options = {
+            storageOptions: {
+                skipBackup: true,
+                path: 'images',
+            },
+        };
+        launchImageLibrary(options, (response) => {
+            if (response.didCancel) {
+                console.log('User cancelled image picker');
+            } else if (response.error) {
+                console.log('ImagePicker Error: ', response.error);
+            } else if (response.customButton) {
+                console.log('User tapped custom button: ', response.customButton);
+                alert(response.customButton);
+            } else {
+                const source = { uri: response.uri };
+                // console.log('response', JSON.stringify(response));
+                setFilePath(response),
+                    setFileData(response.data),
+                    setFileUri(response.uri)
+
+            }
+        });
+
+    }
+
+    //   SHOW THE PREVIEW OF THE SELECTED IMAGE 
+
+    renderFileUri = () => {
+        if (fileUri) {
+            return <Image
+                source={{ uri: fileUri }}
+                style={styles.images}
+            />
+        } else {
+            <Text>Nothing to show here</Text>
+        }
+    }
+
+    // CALLING THE FUNCTION TO SELECT THE IMAGE FROM THE GALLERY
+
+    const renderComposer = props => {
+        return (
+            <View style={{ flexDirection: 'row', padding: 5 }}>
+                <Composer {...props} />
+              <Ionicons name='attach' size = {35} color = "black" onPress={launch} />
+            </View>
+        );
+    }
 
     return (
         <GiftedChat
@@ -122,8 +174,9 @@ export default function RoomScreen({ route }) {
             placeholder="Type a message..."
             showUserAvatar={true}
             showAvatarForEveryMessage={true}
-            // renderComposer={this.renderComposer}
+            renderComposer={renderComposer}            
         />
+
     );
 }
 
@@ -146,4 +199,26 @@ const styles = StyleSheet.create({
         color: 'black',
         fontWeight: 'bold'
     },
+    images: {
+        width: 150,
+        height: 150,
+        borderColor: 'black',
+        borderWidth: 1,
+        marginHorizontal: 3
+    },
+    btnSection: {
+        width: 225,
+        height: 50,
+        backgroundColor: '#DCDCDC',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 3,
+        marginBottom: 10
+    },
+    btnText: {
+        textAlign: 'center',
+        color: 'gray',
+        fontSize: 14,
+        fontWeight: 'bold'
+    }
 });
